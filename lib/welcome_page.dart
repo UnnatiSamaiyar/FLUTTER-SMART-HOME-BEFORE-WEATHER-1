a9c5ab9be86a655690c58163900c5aa9071e8bac
jaygarg04 <150328981+jaygarg04@users.noreply.github.com>
2024-05-27 08:53:54 +0530

    Update welcome_page.dart

--- lib/welcome_page.dart
+++ lib/welcome_page.dart
@@ -11,6 +11,14 @@ class HomePage extends StatefulWidget {
     required this.humidityApi,
     required this.baseurlApi,
     required this.tokenApi,
+    required this.windspeedapi,
+    required this.raincountapi,
+    required this.winddirectionapi,
+    required this.iaqapi,
+    required this.co2api,
+    required this.breathvocapi,
+    required this.pressureapi,
+    required this.dewpointapi,
   }) : super(key: key);
 
   final String temperatureApi;
@@ -19,6 +27,14 @@ class HomePage extends StatefulWidget {
   final String humidityApi;
   final String baseurlApi;
   final String tokenApi;
+  final String windspeedapi;
+  final String raincountapi;
+  final String winddirectionapi;
+  final String iaqapi;
+  final String co2api;
+  final String breathvocapi;
+  final String pressureapi;
+  final String dewpointapi;
 
   @override
   _HomePageState createState() => _HomePageState();
@@ -61,31 +77,36 @@ class _HomePageState extends State<HomePage> {
               ),
               Expanded(
                 child: ListView(
-                physics: const BouncingScrollPhysics(),
-                children: [
-                  const SizedBox(height: 20),
-                  Center(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        SizedBox(
-                          width: 150,   // Set the desired width for the first logo
-                          height: 150,  // Set the desired height for the first logo
-                          child: Image.asset(
-                            'assets/images/tatasteellogo.png',
+                  physics: const BouncingScrollPhysics(),
+                  children: [
+                    const SizedBox(height: 20),
+                    Center(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            width:
+                                150, // Set the desired width for the first logo
+                            height:
+                                150, // Set the desired height for the first logo
+                            child: Image.asset(
+                              'assets/images/tatasteellogo.png',
+                            ),
                           ),
-                        ),
-                        SizedBox(width: 20),  // Add some spacing between the logos
-                        SizedBox(
-                          width: 150,   // Set the desired width for the second logo
-                          height: 150,  // Set the desired height for the second logo
-                          child: Image.asset(
-                            'assets/images/nabardlogo.jpeg',  // Replace with your second logo path
+                          const SizedBox(
+                              width: 20), // Add some spacing between the logos
+                          SizedBox(
+                            width:
+                                150, // Set the desired width for the second logo
+                            height:
+                                150, // Set the desired height for the second logo
+                            child: Image.asset(
+                              'assets/images/nabardlogo.jpeg', // Replace with your second logo path
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
-                  ),
                     const SizedBox(height: 48),
                     const Text(
                       'SERVICES',
@@ -116,12 +137,46 @@ class _HomePageState extends State<HomePage> {
                               context,
                               MaterialPageRoute(
                                 builder: (context) => TemperaturePage(
-                                  temperatureApi: widget.temperatureApi,
-                                  tdsApi: widget.tdsApi,
-                                  humidityApi: widget.humidityApi,
-                                  phApi: widget.phApi,
+                                  temperatureApi:
+                                      widget.temperatureApi.isNotEmpty
+                                          ? widget.temperatureApi
+                                          : 'default_temperature_api',
+                                  tdsApi: widget.tdsApi.isNotEmpty
+                                      ? widget.tdsApi
+                                      : 'default_tds_api',
+                                  humidityApi: widget.humidityApi.isNotEmpty
+                                      ? widget.humidityApi
+                                      : 'default_humidity_api',
+                                  phApi: widget.phApi.isNotEmpty
+                                      ? widget.phApi
+                                      : 'default_ph_api',
                                   baseurlApi: widget.baseurlApi,
                                   tokenApi: widget.tokenApi,
+                                  windspeedapi: widget.windspeedapi.isNotEmpty
+                                      ? widget.windspeedapi
+                                      : 'default_windspeed_api',
+                                  raincountapi: widget.raincountapi.isNotEmpty
+                                      ? widget.raincountapi
+                                      : 'default_raincount_api',
+                                  winddirectionapi:
+                                      widget.winddirectionapi.isNotEmpty
+                                          ? widget.winddirectionapi
+                                          : 'default_winddirection_api',
+                                  iaqapi: widget.iaqapi.isNotEmpty
+                                      ? widget.iaqapi
+                                      : 'default_iaq_api',
+                                  co2api: widget.co2api.isNotEmpty
+                                      ? widget.co2api
+                                      : 'default_co2_api',
+                                  breathvocapi: widget.breathvocapi.isNotEmpty
+                                      ? widget.breathvocapi
+                                      : 'default_breathvoc_api',
+                                  pressureapi: widget.pressureapi.isNotEmpty
+                                      ? widget.pressureapi
+                                      : 'default_pressure_api',
+                                  dewpointapi: widget.dewpointapi.isNotEmpty
+                                      ? widget.dewpointapi
+                                      : 'default_dewpoint_api',
                                 ),
                               ),
                             );
@@ -148,25 +203,47 @@ class _HomePageState extends State<HomePage> {
                       ],
                     ),
                     const SizedBox(height: 28),
+                    // Display the API values at the bottom
+                    const SizedBox(height: 28),
+                    const Text(
+                      'API Values:',
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    if (widget.temperatureApi.isNotEmpty)
+                      Text('Temperature API: ${widget.temperatureApi}'),
+                    if (widget.tdsApi.isNotEmpty)
+                      Text('TDS API: ${widget.tdsApi}'),
+                    if (widget.phApi.isNotEmpty)
+                      Text('pH API: ${widget.phApi}'),
+                    if (widget.humidityApi.isNotEmpty)
+                      Text('Humidity API: ${widget.humidityApi}'),
+                    if (widget.baseurlApi.isNotEmpty)
+                      Text('Base URL API: ${widget.baseurlApi}'),
+                    if (widget.tokenApi.isNotEmpty)
+                      Text('Token API: ${widget.tokenApi}'),
+                    if (widget.windspeedapi.isNotEmpty)
+                      Text('WindSpeed API: ${widget.windspeedapi}'),
+                    if (widget.raincountapi.isNotEmpty)
+                      Text('raincount API: ${widget.raincountapi}'),
+                    if (widget.winddirectionapi.isNotEmpty)
+                      Text('winddirection API: ${widget.winddirectionapi}'),
+                    if (widget.iaqapi.isNotEmpty)
+                      Text('iaq API: ${widget.iaqapi}'),
+                    if (widget.co2api.isNotEmpty)
+                      Text('co2 API: ${widget.co2api}'),
+                    if (widget.breathvocapi.isNotEmpty)
+                      Text('breathvoc API: ${widget.breathvocapi}'),
+                    if (widget.pressureapi.isNotEmpty)
+                      Text('pressure API: ${widget.pressureapi}'),
+                    if (widget.dewpointapi.isNotEmpty)
+                      Text('dewpoint API: ${widget.dewpointapi}'),
                   ],
                 ),
               ),
-              // Display the API values at the bottom
-              /* const SizedBox(height: 28),
-              const Text(
-                'API Values:',
-                style: TextStyle(
-                  fontSize: 14,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 8),
-              Text('Temperature API: ${widget.temperatureApi}'),
-              Text('TDS API: ${widget.tdsApi}'),
-              Text('pH API: ${widget.phApi}'),
-              Text('Humidity API: ${widget.humidityApi}'),
-              Text('baseurl API: ${widget.baseurlApi}'),
-              Text('token API: ${widget.tokenApi}'),*/
             ],
           ),
         ),
